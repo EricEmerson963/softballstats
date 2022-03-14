bd1950f8d9271dd483ead0dc8d612516e3fc92bc
Eric Emerson <EricSampson@zoho.com>
2022-03-13 21:54:08 -0400

    Minor adjustments

--- src/index.js
+++ src/index.js
@@ -4,12 +4,28 @@ import VeeValidate from 'vee-validate';
 import { store } from './_store';
 import { router } from './_helpers';
 import App from './app/App';
+const sqlite3 = require('sqlite3').verbose();
+
 
 Vue.use(VeeValidate);
 
-// setup fake backend
-import { configureFakeBackend } from './_helpers';
-configureFakeBackend();
+// setup SQLite3 connection
+let db = new sqlite3.Database(':memory:', (err) => {
+	if (err) {
+		return console.error(err.message);
+	}
+
+	console.log('Connected to the SQLite Database');
+});
+
+//Closing database connection
+db.close((err) => {
+	if (err) {
+		return console.error(err.message);
+	}
+
+	console.log('Close the database connection.');
+});
 
 new Vue({
     el: '#app',
