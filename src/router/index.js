14290271b634951ae0c6ab6995116aa976ed3120
Eric Emerson <EricSampson@zoho.com>
2022-03-19 21:08:58 -0400

    Built a statistics route, and view. Next will be a login function to edit the page

--- src/router/index.js
+++ src/router/index.js
@@ -16,6 +16,12 @@ const router = createRouter({
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/AboutView.vue')
+    },
+    {
+      path: '/stats',
+      name: 'stats',
+	// Statistics route
+      component: () => import('../views/StatsView.vue')
     }
   ]
 })
